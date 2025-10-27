c42112ecbbe71f57fcf385207353a53e22d3e34f
aayusht200 <aayush.t200@gmail.com>
2025-10-27 18:35:38 +0530

    added display for winner now working on reset button

--- displayController.js
+++ displayController.js
@@ -1,22 +1,49 @@
 import { gameController } from "./gameController.js";
 import { gameBoard } from "./gameBoard.js";
-import { players } from "./player.js";
-
-const button_grid = document.getElementById("button_grid");
-const playbuttons = document.getElementsByClassName("play_button");
-export function createBoard() {
-  for (let i = 0; i < 9; i++) {
-    const div = document.createElement("button");
-    div.dataset.id = i;
-    button_grid.appendChild(div);
-    div.classList.add("play_button");
-  }
-}
-
 export function displayController() {
-  let game = gameController();
-  function renderBoard() {
-    console.log(playbuttons);
+  const display = document.getElementById("display");
+  const reset = document.getElementById("reset");
+  let score = "";
+  const game = gameController();
+  function createGrid() {
+    for (let i = 0; i < 9; i++) {
+      const div = document.createElement("button");
+      div.dataset.id = i;
+      button_grid.appendChild(div);
+      div.classList.add("play_button");
+      div.addEventListener("click", (e) => {
+        score = game.playRound(i);
+        renderBoard();
+        if (score != "") displayArea(score);
+      });
+    }
   }
-  return { renderBoard };
+  function renderBoard() {
+    const playbuttons = document.getElementsByClassName("play_button");
+    let board = game.getBoard();
+    for (let i = 0; i < board.length; i++) {
+      playbuttons[i].textContent = board[i] ? board[i] : "";
+    }
+  }
+  function displayArea(score) {
+    switch (score) {
+      case "playerOne":
+        display.textContent = "Player One Wins";
+        break;
+      case "playerTwo":
+        display.textContent = "Player Two Wins";
+        break;
+      case "Draw":
+        display.textContent = "Draw!";
+        break;
+      default:
+        display.textContent = "";
+    }
+  }
+  reset.addEventListener("click", (e) => {
+    console.log(game.getBoard());
+  });
+  return { renderBoard, createGrid };
 }
+
+// console.log("test");
--- script.js
+++ script.js
@@ -2,9 +2,8 @@ import * as Condition from "./winCondition.js";
 import { gameBoard } from "./gameBoard.js";
 import { players } from "./player.js";
 import { gameController } from "./gameController.js";
-import { createBoard, displayController } from "./displayController.js";
+import { displayController } from "./displayController.js";
 
-createBoard();
 let game = displayController();
 
-console.log(game.renderBoard());
+game.createGrid();
